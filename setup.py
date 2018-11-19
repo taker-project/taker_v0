5646a03ab498aaf4120326c67fd2bedd4f6333c5
Alexander Kernozhitsky <sh200105@mail.ru>
2018-11-20 01:23:57 +0300

    Change version number to fix the warning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taker',
-    version='0.v0.0',
+    version='0.0.0.a0',
     packages=find_packages(),
     install_requires=['pytest'],
 ) 
