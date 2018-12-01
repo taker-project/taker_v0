74c10f9ab257385fd90f50e7b18e182e12fab38b
Alexander Kernozhitsky <sh200105@mail.ru>
2018-12-01 23:42:45 +0300

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
 setup(
     name='taker',
     version='0.0.0.a0',
+    author='Alexander Kernozhitsky',
+    author_email='sh200105@mail.ru',
+    description='Taker is a system to prepare competitive programming '
+                'problems',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/taker-project/taker_v0',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: ',
+        'GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3'
+    ],
     packages=find_packages(),
     install_requires=['namedlist'],
 )
