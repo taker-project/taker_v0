dcaba7e54821e0299a88365fb0003422dfa2fa5b
Alexander Kernozhitsky <sh200105@mail.ru>
2019-01-21 21:29:59 +0300

    Fix pylint invocation

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -9,7 +9,7 @@ for DIR in src/*; do
   git check-ignore -q "$DIR" && continue
   pylint "$DIR" -d C0111 -d W0511 || OK=1
   [[ -d "$DIR/tests" ]] || continue
-  pylint "$DIR/tests" -d C0111 -d W0511 -d W0614 -d W0621 || OK=1
+  ( cd src && pylint "../$DIR/tests" -d C0111 -d W0511 -d W0614 -d W0621 ) || OK=1
 done
 
 exit "$OK"
