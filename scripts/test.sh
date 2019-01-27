ff103e43076be7607607148c061dd779640d7c28
Alexander Kernozhitsky <sh200105@mail.ru>
2019-01-28 00:38:19 +0300

    Attempt to add codecov

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,4 +8,18 @@ else
   PYCODESTYLE_OPTION=--pep8
 fi
 
-pytest "${PYCODESTYLE_OPTION}"
+COV_OPTIONS=""
+
+if [ -n "${COVERAGE}" ]; then
+  for DIR in src/*; do
+    [ -d "$DIR" ] || continue
+    [ -f "$DIR/__init__.py" ] || continue
+    git check-ignore -q "$DIR" && continue
+    COV_OPTIONS="${COV_OPTIONS} --cov=$(echo "$DIR" | sed 's#^src/##')"
+  done
+fi
+
+echo "${COV_OPTIONS}"
+
+# shellcheck disable=SC2086
+pytest "${PYCODESTYLE_OPTION}" ${COV_OPTIONS} src/*/
