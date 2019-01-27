b8c73e98bca1c4ece7e2fab4c0016dc910bdf50a
Alexander Kernozhitsky <sh200105@mail.ru>
2019-01-27 15:10:43 +0300

    Drop python 3.4 support

--- setup.py
+++ setup.py
@@ -19,7 +19,10 @@ setup(
         'License :: OSI Approved :: '
         'GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     package_dir={'': 'src'},
     packages=find_packages('src'),
