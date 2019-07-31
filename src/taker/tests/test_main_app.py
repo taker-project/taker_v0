b7d9a52f1338d56555f0dfafbd4634caeeaaa792
Alexander Kernozhitsky <sh200105@mail.ru>
2019-07-31 17:25:01 +0300

    Fix lack of check_output in subprocess.run in Py <= 3.6

--- src/taker/tests/test_main_app.py
+++ src/taker/tests/test_main_app.py
@@ -1,6 +1,8 @@
 import subprocess
+from subprocess import PIPE
 import pytest
 import os
+from compat import fspath
 from ...pytest_fixtures import *
 
 
@@ -21,13 +23,13 @@ def make_badsource():
 
 
 def test_help():
-    res = subprocess.run(['take', '-h'], capture_output=True, check=True,
-                         universal_newlines=True)
+    res = subprocess.run(['take', '-h'], stdout=PIPE, stderr=PIPE,
+                         check=True, universal_newlines=True)
     assert res.stdout.find('usage:') >= 0
 
 
 def test_no_command():
-    res = subprocess.run(['take'], capture_output=True,
+    res = subprocess.run(['take'], stdout=PIPE, stderr=PIPE,
                          universal_newlines=True)
     assert res.returncode != 0
     assert res.stderr.find('no command specified') >= 0
@@ -36,22 +38,22 @@ def test_no_command():
 def test_no_task_dir(tmpdir, monkeypatch):
     monkeypatch.chdir(tmpdir)
     make_source()
-    res = subprocess.run(['take', 'compile', 'file.cpp'], capture_output=True,
-                         universal_newlines=True)
+    res = subprocess.run(['take', 'compile', 'file.cpp'],
+                         stdout=PIPE, stderr=PIPE, universal_newlines=True)
     assert res.returncode != 0
     assert res.stderr.find('you must be in task directory') >= 0
 
 
 def test_compile(task_manager, monkeypatch):
-    monkeypatch.chdir(task_manager.repo.directory)
+    monkeypatch.chdir(fspath(task_manager.repo.directory))
     make_source()
     make_badsource()
-    res = subprocess.run(['take', 'compile', 'file.cpp'], capture_output=True,
-                         universal_newlines=True)
+    res = subprocess.run(['take', 'compile', 'file.cpp'],
+                         stdout=PIPE, stderr=PIPE, universal_newlines=True)
     assert res.returncode == 0
     assert res.stdout.find('ok') >= 0
 
-    res = subprocess.run(['take', 'compile', 'bad.cpp'], capture_output=True,
-                         universal_newlines=True)
+    res = subprocess.run(['take', 'compile', 'bad.cpp'],
+                         stdout=PIPE, stderr=PIPE, universal_newlines=True)
     assert res.returncode != 0
     assert res.stdout.find('compilation error') >= 0
