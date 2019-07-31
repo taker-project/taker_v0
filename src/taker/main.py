0d8ce6758ff0270fe16012ab4e20dd8773fe7863
Alexander Kernozhitsky <sh200105@mail.ru>
2019-07-31 13:53:22 +0300

    Write the code of main module

--- src/taker/main.py
+++ src/taker/main.py
@@ -1,2 +1,15 @@
+from cli import ConsoleApp, app, register_app
+
+
+class TakerApp(ConsoleApp):
+    def __init__(self):
+        super().__init__('take')
+        self.parser.description = \
+            'Taker - a task preparation system for competitive programming'
+
+
+register_app(TakerApp())
+
+
 def main():
-    print('Empty :(')
+    app().run()
