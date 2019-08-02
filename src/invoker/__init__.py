74120215052031c42356e93660bf0abdd25c9556
Alexander Kernozhitsky <sh200105@mail.ru>
2019-08-02 21:18:25 +0300

    Test "take run" subcommand

--- src/invoker/__init__.py
+++ src/invoker/__init__.py
@@ -6,3 +6,4 @@ from .profiled_runner import register_profile, create_profile, list_profiles
 from .profiled_runner import AbstractRunProfile
 from .sourcecode import SourceCode
 from .cli import CompileSubcommand, RunSubcommand
+from .utils import default_exe_ext
